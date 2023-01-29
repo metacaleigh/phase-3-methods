22f039b85f01caca3580b44073111219ca08a27c
caleigh <caleighsteill@outlook.com>
2023-01-29 15:07:35 -0700

    commit tree f1ce45d1699c8c130242159b81c312de403b4049

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2,5)
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    num/2
+end
+
+halve(6)
+halve('six')
